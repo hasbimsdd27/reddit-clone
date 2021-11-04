fc0c66df49f812658edfed52765c2c6931e81643
Hasbi Musaddad <musaddadhasbi@gmail.com>
2021-11-04 14:01:17 +0700

    2. setup graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,38 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolvers } from "./resolvers/hello";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+} from "apollo-server-core";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  //   const post = orm.em.create(Post, { title: "my first post" });
-  //   await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
+  const app = express();
 
-  console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolvers],
+      validate: false,
+    }),
+    plugins: [
+      __prod__
+        ? ApolloServerPluginLandingPageDisabled()
+        : ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
